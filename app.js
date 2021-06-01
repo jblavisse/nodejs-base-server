75c368378338ede919650c72d36e58dac3fb4216
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-01 10:16:26 +0200

    Shortcut node.JS for writehead and end response

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const http = require('http');
 
 const server = http.createServer();
+const port = 4400;
 
 server.on('request', (request, response) => {
     // Type et code status de la réponse
-    response.setHeader('Content-Type','text-plain');
-    response.writeHead(200);
-
-    // Ecrire le contenu de la réponse
-    response.write('Hello World!');
+    response.writeHead(200, {
+        'Content-Type': 'text-plain'
+    });
 
     // Envoyer ma réponse complète
-    response.end();
+    response.end('Hello World!');
 
 })
 
-server.listen(5500, "localhost", (err) => {
+server.listen(port, "localhost", (err) => {
     if(err) console.log(err);
     else {
-        console.log("Server is running on http://localhost:5500");
+        console.log(`Server is running on http://localhost:${port}`);
     }
 })
