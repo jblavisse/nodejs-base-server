df9df420df8f04e5667ec0df82cb40ced0e3516e
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-01 10:59:13 +0200

    Add routing system for contact and style.css

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer();
-const port = 4400;
+const port = 7000;
 
 server.on('request', (request, response) => {
-    // Type et code status de la réponse
-    response.writeHead(200, {
-        'Content-Type': 'text-plain'
-    });
+    console.log(request.url);
 
-    // Envoyer ma réponse complète
-    response.end('Hello World!');
+    if(request.url === "/") {
+        // La réponse lorsqu'on est sur http://localhost:port/
+        // Type et code status de la réponse
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+
+        fs.readFile('index.html','utf-8',(err, fileContent) => {
+            if(err) console.log(err);
+            else response.end(fileContent);
+        })
+    }
+    else if(request.url === "/contact") {
+        // La réponse lorsqu'on est sur http://localhost:port/contact
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+
+        response.end('<html><body><h1>Ma page de contact</h1></body></html>')
+    }
+    else if(request.url === "/style.css") {
+        response.writeHead(200, {
+            'Content-Type': 'text/css'
+        });
+
+        fs.readFile('style.css','utf-8',(err, fileContent) => {
+            if(err) console.log(err);
+            else response.end(fileContent);
+        })
+    }
+    else {
+        // La réponse envoyée pour toutes les autres URL
+        response.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+
+        response.end('<html><body><h1>404 Not found!</h1></body></html>')  
+    }
 
 })
 
